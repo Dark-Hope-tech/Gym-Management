173f7356b9ca65d7182b0d8399f189d45d5fbf24
Tabish Malik <76650437+Dark-Hope-tech@users.noreply.github.com>
2021-11-25 13:37:36 +0530

    added project
    added project

---
+++ src/gym/test.java
@@ -0,0 +1,13 @@
+package gym;
+import java.io.*;
+public class test {
+    public static void main(String[] args) {
+       Print();
+    }
+    public static void Print() {
+        int i;
+        i=5;
+        System.out.println(i);
+        System.out.println(5);
+    }
+}
